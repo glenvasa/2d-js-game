d0b2ae2fc7fe679b553fe8c5211d77c48e7890b7
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-22 16:28:53 -0500

    created Player class to draw player image with spritesheet

--- script.js
+++ script.js
@@ -39,7 +39,28 @@ window.addEventListener('load', () => {
     // reacts to keyboard events from InputHandler,
     // draws and updates the player
     class Player {
-
+        constructor(gameWidth, gameHeight){
+   // so that player doesn't run off screen 
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 200;
+            this.height = 200;
+            this.x = 0;
+            this.y = this.gameHeight - this.height;
+            this.image = document.getElementById('playerImage')
+            this.frameX = 0;
+            this.frameY = 0;
+        }
+        draw(context){
+            context.fillStyle = 'white'
+            context.fillRect(this.x, this.y, this.width, this.height)
+            // arguments 2,3,4,5 are sourcex, sy, sw, sh are the source image properties (spritesheet)
+            // 0,0 for sx, sy give us top left image in spritesheet
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
+        }
+        update(){
+            // this.x++
+        }
     }
 
     // handles endlessly scrolling backgrounds
@@ -64,11 +85,18 @@ window.addEventListener('load', () => {
 
 
     const input = new InputHandler()
+    const player = new Player(canvas.width, canvas.height)
+    
+    
 
     // main animation loop; runs 60x/second
     function animate() {
-
+        ctx.clearRect(0,0, canvas.width, canvas.height)
+        player.update()
+        player.draw(ctx)
+        requestAnimationFrame(animate)
     }
 
-
+    animate()
 })
+
